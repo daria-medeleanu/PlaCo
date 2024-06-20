91803bd2267bce9118a0b2989de9fd79de34cf98
BiancaVulpescu <126167993+BiancaVulpescu@users.noreply.github.com>
2024-06-20 20:55:23 +0300

    paths

--- frontend/FreelancerLoggedIn/search_for_jobs/search_for_jobs.php
+++ frontend/FreelancerLoggedIn/search_for_jobs/search_for_jobs.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search for jobs</title>
-    <link rel="stylesheet" href="../header/header.css"> 
-    <link rel="stylesheet" href="style/search_for_jobs.css"> 
-    <link rel="shortcut icon" type="image/x-icon" href="logo.png">
+    <link rel="stylesheet" href="/PlaCo/frontend/FreelancerLoggedIn/header/header.css"> 
+    <link rel="stylesheet" href="/PlaCo/frontend/FreelancerLoggedIn/search_for_jobs/style/search_for_jobs.css"> 
+    <link rel="shortcut icon" type="image/x-icon" href="/PlaCo/frontend/FreelancerLoggedIn/search_for_jobs/logo.png">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -14,23 +14,23 @@
 
     <div class="header">
         <div class="nav-left">
-            <a class="logo-pic" href="../../Login/DashboardLogin.html">
-                <img src="./logo.png" class="logo" alt="Logo">
+            <a class="logo-pic" href="/home/home">
+                <img src="/PlaCo/frontend/FreelancerLoggedIn/search_for_jobs/logo.png" class="logo" alt="Logo">
                 <div class="nav-btn-left">PlaCo</div>
             </a>
         </div>
         <div class="nav-right">
             <div class="options-nav-bar">
-                <a href="../search_for_jobs/search_for_jobs.html" class="nav-btn-left">Search for jobs</a>
+                <a href="/home/search_for_jobs" class="nav-btn-left">Search for jobs</a>
             </div>
             <div class="menu-btn-right btn-dissapear">
                 <input type="checkbox" id="profile-toggle">
                 <label for="profile-toggle" >Profile</label>
                 <div class="menu" id="profile-menu">
-                    <button onclick="window.location.href='../freelancer_profile/freelancer_profile.html'">My Profile</button>
-                    <button onclick="window.location.href='../freelancer_profile/my_portfolio.html'">My Portfolio</button>
-                    <button onclick="window.location.href='../../Login/DashboardLogin.html'">Log Out</button>
-                    <button onclick="window.location.href='../settings/settings.html'">Settings</button>
+                    <button onclick="window.location.href='/home/freelancer_profile'">My Profile</button>
+                    <button onclick="window.location.href='/home/my_portfolio'">My Portfolio</button>
+                    <button onclick="window.location.href='/home/home'">Log Out</button>
+                    <button onclick="window.location.href='/home/settings_freelancer'">Settings</button>
                 </div>
             </div>
         </div>
@@ -67,7 +67,7 @@
             <form class="search-container">
                 <input type="text" placeholder="Search...">
                 <button type="submit">
-                    <img src="./search2.png" alt="Search">
+                    <img src="/PlaCo/frontend/FreelancerLoggedIn/search_for_jobs/search2.png" alt="Search">
                 </button>
             </form>
             <div class="filters-box">
@@ -97,7 +97,7 @@
         </div>
         <div class="main-content">
             <div class="projects-list">
-                <a href="project.html" class="project-box">
+                <a href="/home/project" class="project-box">
                     <div class="project-text-input">
                         <h2>Project name</h2>
                         <div class="text-container">
@@ -122,7 +122,7 @@
                          </div>
                     </div>
                 </a>
-                <a href="project.html" class="project-box">
+                <a href="/home/project" class="project-box">
                         <div class="project-text-input">
                             <h2>Project name</h2>
                             <div class="text-container">
